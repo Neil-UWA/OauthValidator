93cdec74e5a757b8ed3dcc4688ab66d69f318fca
Neil <neil-lu@graduate.uwa.edu.au>
2016-08-05 11:27:23 +0800

    use es5 syntax

--- oauth-validator.js
+++ oauth-validator.js
@@ -2,71 +2,68 @@
 let Promise = require('bluebird');
 let request = Promise.promisifyAll(require('request'));
 
-class OauthValidator {
+function OauthValidator(service) {
+  this.service = service;
+  this.appid = 'xxxx';
+  this.options = {json: true};
+};
 
-  constructor(service) {
-    this.service = service;
-    this.appid = 'wx1093f8d7a911a1ef',
-    this.options = { json: true };
+OauthValidator.prototype.getIdentity =function(data) {
+  let identity = undefined;
+
+  try {
+    identity = data.uid ||
+      data.openid ||
+      JSON.parse(data.replace('callback(', '').replace(');', '')).openid;
+  } catch(e) {
+    return undefined;
   }
+  return identity;
+};
 
-  getIdentity(data) {
-    let identity = undefined;
+OauthValidator.prototype.validate = function(token, expectedIdentity) {
+  this.buildOptions(token);
+  let method = this.service === 'weibo' ? 'postAsync' : 'getAsync';
 
-    try {
-      identity = data.uid ||
-                 data.openid ||
-                 JSON.parse(data.replace('callback(', '').replace(');', '')).openid;
-    } catch(e) {
-      return undefined;
+  return request[method](this.options).spread((res, body)=>{
+    const identity = this.getIdentity(body);
+
+    //weibo uid is number there for use `==` rather than `===`
+    if (identity) {
+      return identity == expectedIdentity;
+    } else {
+      return Promise.reject(body);
     }
-    return identity;
-  }
+  });
+};
 
-  validate(token, expectedIdentity) {
-    this.buildOptions(token);
-    let method = this.service === 'weibo' ? 'postAsync' : 'getAsync';
-
-    return request[method](this.options).spread((res, body)=>{
-      const identity = this.getIdentity(body);
-
-      //weibo uid is number there for use `==` rather than `===`
-      if (identity) {
-        return identity == expectedIdentity;
-      } else {
-        return Promise.reject(body);
-      }
-    });
-  }
-
-  getEndpoint() {
+OauthValidator.prototype.getEndpoint = function() {
     switch(this.service) {
       case 'qq': return OauthValidator.QQ_ENDPOINT;
       case 'wechat': return OauthValidator.WECHAT_ENDPOINT;
       case 'weibo': return OauthValidator.WEIBO_ENDPIONT;
       default: return undefined;
     };
-  }
+}
 
-  buildOptions(token) {
-    this.options.uri = this.getEndpoint();
-    switch(this.service) {
-      case 'weibo':
-        this.options.form = {access_token: token};
-        break;
-      case 'wechat':
-        this.options.qs = {
-          appid: this.appid,
-          grant_type: 'refresh_token',
-          refresh_token: token
-        };
-        break;
-      case 'qq':
-        this.options.qs = {access_token: token};
-        break;
+OauthValidator.prototype.buildOptions = function(token) {
+  this.options.uri = this.getEndpoint();
+  switch(this.service) {
+    case 'weibo':
+      this.options.form = {access_token: token};
+    break;
+    case 'wechat':
+      this.options.qs = {
+      appid: this.appid,
+      grant_type: 'refresh_token',
+      refresh_token: token
     };
-    return this.options;
-  }
+    break;
+    case 'qq':
+      this.options.qs = {access_token: token};
+    break;
+  };
+  return this.options;
 }
 
 OauthValidator.QQ_ENDPOINT     = 'https://graph.qq.com/oauth2.0/me';
