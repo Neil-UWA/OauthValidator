ce20fc7a8cf7dec00d8be3124b4456d22a48468b
Neil <neil-lu@graduate.uwa.edu.au>
2017-09-07 09:08:34 +0800

    fix tests, passing config to constructor

--- oauth-validator.js
+++ oauth-validator.js
@@ -2,20 +2,19 @@
 let Promise = require('bluebird');
 let request = Promise.promisifyAll(require('request'));
 
-function OauthValidator(service) {
-  this.service = service;
-  this.appid = 'xxxx';
-  this.options = {json: true};
-};
+function OauthValidator(config) {
+  config = config || {};
+  this.service = config.service;
+  this.appid = config.appid;
+  this.options = { json: true };
+}
 
-OauthValidator.prototype.getIdentity =function(data) {
+OauthValidator.prototype.getIdentity = function(data) {
   let identity = undefined;
 
   try {
-    identity = data.uid ||
-      data.openid ||
-      JSON.parse(data.replace('callback(', '').replace(');', '')).openid;
-  } catch(e) {
+    identity = data.uid || data.openid || JSON.parse(data.replace('callback(', '').replace(');', '')).openid;
+  } catch (e) {
     return undefined;
   }
   return identity;
@@ -25,7 +24,7 @@ OauthValidator.prototype.validate = function(token, expectedIdentity) {
   this.buildOptions(token);
   let method = this.service === 'weibo' ? 'postAsync' : 'getAsync';
 
-  return request[method](this.options).spread((res, body)=>{
+  return request[method](this.options).spread((res, body) => {
     const identity = this.getIdentity(body);
 
     //weibo uid is number there for use `==` rather than `===`
@@ -38,36 +37,40 @@ OauthValidator.prototype.validate = function(token, expectedIdentity) {
 };
 
 OauthValidator.prototype.getEndpoint = function() {
-    switch(this.service) {
-      case 'qq': return OauthValidator.QQ_ENDPOINT;
-      case 'wechat': return OauthValidator.WECHAT_ENDPOINT;
-      case 'weibo': return OauthValidator.WEIBO_ENDPIONT;
-      default: return undefined;
-    };
-}
+  switch (this.service) {
+    case 'qq':
+      return OauthValidator.QQ_ENDPOINT;
+    case 'wechat':
+      return OauthValidator.WECHAT_ENDPOINT;
+    case 'weibo':
+      return OauthValidator.WEIBO_ENDPIONT;
+    default:
+      return undefined;
+  }
+};
 
 OauthValidator.prototype.buildOptions = function(token) {
   this.options.uri = this.getEndpoint();
-  switch(this.service) {
+  switch (this.service) {
     case 'weibo':
-      this.options.form = {access_token: token};
-    break;
+      this.options.form = { access_token: token };
+      break;
     case 'wechat':
       this.options.qs = {
-      appid: this.appid,
-      grant_type: 'refresh_token',
-      refresh_token: token
-    };
-    break;
+        appid: this.appid,
+        grant_type: 'refresh_token',
+        refresh_token: token
+      };
+      break;
     case 'qq':
-      this.options.qs = {access_token: token};
-    break;
-  };
+      this.options.qs = { access_token: token };
+      break;
+  }
   return this.options;
-}
+};
 
-OauthValidator.QQ_ENDPOINT     = 'https://graph.qq.com/oauth2.0/me';
+OauthValidator.QQ_ENDPOINT = 'https://graph.qq.com/oauth2.0/me';
 OauthValidator.WECHAT_ENDPOINT = 'https://api.weixin.qq.com/sns/oauth2/refresh_token';
-OauthValidator.WEIBO_ENDPIONT  = 'https://api.weibo.com/oauth2/get_token_info';
+OauthValidator.WEIBO_ENDPIONT = 'https://api.weibo.com/oauth2/get_token_info';
 
 module.exports = OauthValidator;
--- test/oauth-validator.test.js
+++ test/oauth-validator.test.js
@@ -7,208 +7,202 @@ let expect = require('chai').expect;
 
 var request = Promise.promisifyAll(require('request'));
 
-const QQ_ENDPOINT     = 'https://graph.qq.com/oauth2.0/me';
+const QQ_ENDPOINT = 'https://graph.qq.com/oauth2.0/me';
 const WECHAT_ENDPOINT = 'https://api.weixin.qq.com/sns/oauth2/refresh_token';
-const WEIBO_ENDPIONT  = 'https://api.weibo.com/oauth2/get_token_info';
+const WEIBO_ENDPIONT = 'https://api.weibo.com/oauth2/get_token_info';
 
-//WIP
 describe('OauthValidator', function() {
-
-  describe('#constructor', ()=>{
-    it('works', ()=>{
-      let oauthValidator = new OauthValidator('qq');
+  describe('#constructor', () => {
+    it('works', () => {
+      let oauthValidator = new OauthValidator({ service: 'qq' });
       expect(oauthValidator.service).to.eql('qq');
       expect(oauthValidator).to.be.an.instanceof(OauthValidator);
-      expect(oauthValidator.options).to.eql({json: true});
+      expect(oauthValidator.options).to.eql({ json: true });
     });
-  })
+  });
 
-  describe('#getIdentity', ()=> {
+  describe('.getIdentity', () => {
     let qq = 'callback( {"client_id": "1234", "openid": "111111"} );';
-    let wechat = {openid: '222222' };
-    let weibo = {uid: '333333'};
+    let wechat = { openid: '222222' };
+    let weibo = { uid: '333333' };
 
-    beforeEach(()=>{
+    beforeEach(() => {
       this.oauthValidator = new OauthValidator();
-    })
+    });
 
-    it('returns uid when login with weibo', ()=>{
+    it('returns uid when login with weibo', () => {
       this.oauthValidator.service = 'weibo';
       expect(this.oauthValidator.getIdentity(weibo)).to.eql('333333');
     });
 
-    it('returns openid when login with qq', ()=>{
+    it('returns openid when login with qq', () => {
       this.oauthValidator.service = 'qq';
       expect(this.oauthValidator.getIdentity(qq)).to.eql('111111');
     });
 
-    it('returns openid when login with wechat', ()=>{
+    it('returns openid when login with wechat', () => {
       this.oauthValidator.service = 'wechat';
       expect(this.oauthValidator.getIdentity(wechat)).to.eql('222222');
     });
 
-    it('should return undefined when data is invalid', ()=>{
-      expect(this.oauthValidator.getIdentity({xxx: 'wrong'})).to.eql(undefined);
+    it('should return undefined when data is invalid', () => {
+      expect(this.oauthValidator.getIdentity({ xxx: 'wrong' })).to.eql(undefined);
     });
   });
 
-  describe('#getEndpoint', ()=>{
+  describe('.getEndpoint', () => {
     [
-      {service: 'wechat', endpoint: OauthValidator.WECHAT_ENDPOINT},
-      {service: 'qq', endpoint: OauthValidator.QQ_ENDPOINT},
-      {service: 'weibo', endpoint: OauthValidator.WEIBO_ENDPIONT}
-    ].forEach(data=>{
-      it(`should return ${data.service} endpoint with service ${data.service}`, ()=>{
-        let oauthValidator = new OauthValidator(data.service);
+      { service: 'wechat', endpoint: OauthValidator.WECHAT_ENDPOINT },
+      { service: 'qq', endpoint: OauthValidator.QQ_ENDPOINT },
+      { service: 'weibo', endpoint: OauthValidator.WEIBO_ENDPIONT }
+    ].forEach(data => {
+      it(`should return ${data.service} endpoint with service ${data.service}`, () => {
+        let oauthValidator = new OauthValidator({ service: data.service });
         expect(oauthValidator.getEndpoint()).to.eql(data.endpoint);
       });
     });
 
-    it('should return undefined when service is unknown', ()=>{
-        let oauthValidator = new OauthValidator('xxx');
-        expect(oauthValidator.getEndpoint()).to.eql(undefined);
+    it('should return undefined when service is unknown', () => {
+      let oauthValidator = new OauthValidator({ service: 'xxx' });
+      expect(oauthValidator.getEndpoint()).to.eql(undefined);
     });
   });
 
-  describe('#buildOptions', ()=>{
-    [{
-      service: 'weibo',
-      token: 'token',
-      options: {
-        json: true,
-        uri: OauthValidator.WEIBO_ENDPIONT,
-        form: {access_token: 'token'}
-      }
-    }, {
-      service: 'wechat',
-      token: 'token',
-      options: {
-        json: true,
-        uri: OauthValidator.WECHAT_ENDPOINT,
-        qs: {
-          appid: 'wx1093f8d7a911a1ef',
-          grant_type: 'refresh_token',
-          refresh_token: 'token'
+  describe('.buildOptions', () => {
+    [
+      {
+        service: 'weibo',
+        token: 'token',
+        options: {
+          json: true,
+          uri: OauthValidator.WEIBO_ENDPIONT,
+          form: { access_token: 'token' }
+        }
+      },
+      {
+        service: 'wechat',
+        token: 'token',
+        options: {
+          json: true,
+          uri: OauthValidator.WECHAT_ENDPOINT,
+          qs: {
+            appid: 'xxx',
+            grant_type: 'refresh_token',
+            refresh_token: 'token'
+          }
+        }
+      },
+      {
+        service: 'qq',
+        token: 'token',
+        options: {
+          json: true,
+          uri: OauthValidator.QQ_ENDPOINT,
+          qs: { access_token: 'token' }
         }
       }
-    }, {
-      service: 'qq',
-      token: 'token',
-      options: {
-        json: true,
-        uri: OauthValidator.QQ_ENDPOINT,
-        qs: { access_token: 'token'}
-      }
-    }].forEach(data=>{
-      it(`build options for ${data.service}`, ()=>{
-        let oauthValidator = new OauthValidator(data.service);
+    ].forEach(data => {
+      it(`build options for ${data.service}`, () => {
+        let oauthValidator = new OauthValidator({ service: data.service });
+        if (data.service === 'wechat') oauthValidator.appid = data.options.qs.appid;
         oauthValidator.buildOptions(data.token);
         expect(oauthValidator.options).to.eql(data.options);
       });
-    })
+    });
   });
 
-  describe('#validate', ()=>{
+  describe('.validate', () => {
     let getStub = null;
     let postStub = null;
 
-    context('login with weibo', ()=>{
-      beforeEach(()=>{
-        postStub =
-          sinon.stub(request, 'postAsync')
-                .returns(Promise.resolve([{}, {uid: 123}]));
+    context('login with weibo', () => {
+      beforeEach(() => {
+        postStub = sinon.stub(request, 'postAsync').returns(Promise.resolve([{}, { uid: 123 }]));
       });
 
-      afterEach(()=> postStub.restore());
+      afterEach(() => postStub.restore());
 
-      it('should call request.postAsync once', ()=>{
-        let oauthValidator = new OauthValidator('weibo');
+      it('should call request.postAsync once', () => {
+        let oauthValidator = new OauthValidator({ service: 'weibo' });
         oauthValidator.validate('token');
         expect(postStub.calledOnce).to.be.true;
       });
 
-      it('should returns true when result matches identity', done=>{
-        let oauthValidator = new OauthValidator('weibo');
-        oauthValidator.validate('token', 123).then(matched=>{
+      it('should returns true when result matches identity', done => {
+        let oauthValidator = new OauthValidator({ service: 'weibo' });
+        oauthValidator.validate('token', 123).then(matched => {
           expect(matched).to.be.true;
           done();
         });
       });
 
-      it('should returns false when result doesn\'t match identity', done=>{
-        let oauthValidator = new OauthValidator('weibo');
-        oauthValidator.validate('token', 122).then(matched=>{
+      it("should returns false when result doesn't match identity", done => {
+        let oauthValidator = new OauthValidator({ service: 'weibo' });
+        oauthValidator.validate('token', 122).then(matched => {
           expect(matched).to.be.false;
           done();
         });
       });
     });
 
-    context('login with wechat', ()=>{
-      beforeEach(()=>{
-        getStub =
-          sinon.stub(request, 'getAsync')
-               .returns(Promise.resolve([{}, {openid: 'xxxx'}]));
+    context('login with wechat', () => {
+      beforeEach(() => {
+        getStub = sinon.stub(request, 'getAsync').returns(Promise.resolve([{}, { openid: 'xxxx' }]));
       });
 
-      afterEach(()=> getStub.restore());
+      afterEach(() => getStub.restore());
 
-      it('should call request.getAsync once', ()=>{
-        let oauthValidator = new OauthValidator('wechat');
+      it('should call request.getAsync once', () => {
+        let oauthValidator = new OauthValidator({ service: 'wechat' });
         oauthValidator.validate('token');
         expect(getStub.calledOnce).to.be.true;
       });
 
-      it('should returns true when result matches identity', done=>{
-        let oauthValidator = new OauthValidator('wechat');
-        oauthValidator.validate('token', 'xxxx').then(matched=>{
+      it('should returns true when result matches identity', done => {
+        let oauthValidator = new OauthValidator({ service: 'wechat' });
+        oauthValidator.validate('token', 'xxxx').then(matched => {
           expect(matched).to.be.true;
           done();
         });
       });
 
-      it('should returns false when result doesn\'t match identity', done=>{
-        let oauthValidator = new OauthValidator('wechat');
-        oauthValidator.validate('token', 'x3x').then(matched=>{
+      it("should returns false when result doesn't match identity", done => {
+        let oauthValidator = new OauthValidator({ service: 'wechat' });
+        oauthValidator.validate('token', 'x3x').then(matched => {
           expect(matched).to.be.false;
           done();
         });
       });
-
     });
 
-    context('login with qq', ()=>{
-      beforeEach(()=>{
-        getStub =
-          sinon.stub(request, 'getAsync')
-               .returns(Promise.resolve([{}, {openid: 'yyyy'}]));
+    context('login with qq', () => {
+      beforeEach(() => {
+        getStub = sinon.stub(request, 'getAsync').returns(Promise.resolve([{}, { openid: 'yyyy' }]));
       });
 
-      afterEach(()=> getStub.restore());
+      afterEach(() => getStub.restore());
 
-      it('should call request.getAsync once', ()=>{
-        let oauthValidator = new OauthValidator('qq');
+      it('should call request.getAsync once', () => {
+        let oauthValidator = new OauthValidator({ service: 'qq' });
         oauthValidator.validate('token');
         expect(getStub.calledOnce).to.be.true;
       });
 
-      it('should returns true when result matches identity', done=>{
-        let oauthValidator = new OauthValidator('qq');
-        oauthValidator.validate('token', 'yyyy').then(matched=>{
+      it('should returns true when result matches identity', done => {
+        let oauthValidator = new OauthValidator({ service: 'qq' });
+        oauthValidator.validate('token', 'yyyy').then(matched => {
           expect(matched).to.be.true;
           done();
         });
       });
 
-      it('should returns false when result doesn\'t match identity', done=>{
-        let oauthValidator = new OauthValidator('qq');
-        oauthValidator.validate('token', 'y3y').then(matched=>{
+      it("should returns false when result doesn't match identity", done => {
+        let oauthValidator = new OauthValidator({ service: 'qq' });
+        oauthValidator.validate('token', 'y3y').then(matched => {
           expect(matched).to.be.false;
           done();
         });
       });
     });
   });
-
 });
-
